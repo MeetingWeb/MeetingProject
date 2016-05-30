647e3bd1296c94a2d1394303d2d4491b72be88f7
duniv5-005 <duniv5-005@duniv5-005-PC>
2016-05-30 11:09:27 +0900

    ddd

--- TeamTest2/src/Test.java
+++ TeamTest2/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("»Æ¿Œ«ﬂ¿Ω-«ı");
+		System.out.println("»Æ¿Œ«ﬂ¿Ω-«ıddd");
 	}
 
 }
