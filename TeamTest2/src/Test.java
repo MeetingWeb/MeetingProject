5ffc6d0f755841c5873d1d43d72ec40ca64b5905
duniv5-010 <duniv5-010@duniv5-010-PC>
2016-05-30 11:32:29 +0900

    줄임

--- TeamTest2/src/Test.java
+++ TeamTest2/src/Test.java
@@ -4,11 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("확인했음-혁ddd");
-		System.out.println("확인했음-준ddd");
-		System.out.println("확인했음-뀨dd");
-		System.out.println("확인했음-뀨2dd");
-		System.out.println("확인했음-뀨3dd");
-		System.out.println("확인했음-뀨4dd");
+		
 	}
 
 }
