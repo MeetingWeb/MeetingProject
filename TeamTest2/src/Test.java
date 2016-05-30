a820c8fcee0c15faeb37909ad38ea7bf53328b17
Jun <duniv5-011@duniv5-011-PC>
2016-05-30 11:56:14 +0900

    we

--- TeamTest2/src/Test.java
+++ TeamTest2/src/Test.java
@@ -5,7 +5,6 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("»Æ¿Œ«ﬂ¿Ω-«ıddd");
 		System.out.println("»Æ¿Œ«ﬂ¿Ω-≤Û23dd");
-		System.out.println("»Æ¿Œ«ﬂ¿Ω-≤Û3dd");
 		
 	}
 
