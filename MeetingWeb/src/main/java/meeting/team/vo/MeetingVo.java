d86ace84e1f445d08ac66b98571aef8caf752909
kimhyeongjun <junkim0312@gmail.com>
2016-06-01 18:32:38 +0900

    Add files via upload

---
+++ MeetingWeb/src/main/java/meeting/team/vo/MeetingVo.java
@@ -0,0 +1,88 @@
+package meeting.team.vo;
+
+import java.sql.Date;
+
+public class MeetingVo {
+	private String title;
+	private String meetingType;
+	private String contents;
+	private String master;
+	private String area;
+	private Date start_time;
+	private Date end_time;
+	private String map_name;
+	private String division;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMeetingType() {
+		return meetingType;
+	}
+
+	public void setMeetingType(String meetingType) {
+		this.meetingType = meetingType;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getMaster() {
+		return master;
+	}
+
+	public void setMaster(String master) {
+		this.master = master;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public Date getStart_time() {
+		return start_time;
+	}
+
+	public void setStart_time(Date start_time) {
+		this.start_time = start_time;
+	}
+
+	public Date getEnd_time() {
+		return end_time;
+	}
+
+	public void setEnd_time(Date end_time) {
+		this.end_time = end_time;
+	}
+
+	public String getMap_name() {
+		return map_name;
+	}
+
+	public void setMap_name(String map_name) {
+		this.map_name = map_name;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+
+	public void setDivision(String division) {
+		this.division = division;
+	}
+
+}
